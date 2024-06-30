db3a955325b762711070b83bfe9db011d793bdbd
lusan23 <luanlol450@gmail.com>
2024-06-30 15:33:45 -0300

    now the recors script is triggred by WM_QUERYSESSION or WM_ENDSESSION, and a rts_started flag to avoid duplicated records

--- background_process.py
+++ background_process.py
@@ -4,27 +4,33 @@ import win32gui
 import record_time_script as rts
 import datetime
 
+# Flag to indicate if rts.start() has been called
+rts_called = False
+
 def wnd_proc(hwnd, msg, wparam, lparam):
+    global rts_called
+    message_map = [win32con.WM_QUERYENDSESSION, win32con.WM_ENDSESSION]
+
     # Get current date and time
     current_datetime = datetime.datetime.now()
 
     # Format date and time as string
     datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"windows message:{str(msg)}, {datetime_str}")
-    
-    if msg == 800:
-    #if msg == win32con.WM_QUERYENDSESSION:
-        # HANDLE THE SHUTDOWN REQUEST HERE
-        rts.start()
-        return True # return true to allow shutdown, false to deny it
-    
-    elif msg == win32con.WM_ENDSESSION:
-        # Clean up if needed
-        if wparam: #wparam is true if session is ending
-            print("WM_ENDSESSION received, session ending")
-        else:
-            print("WM_ENDSESSION received, session not ending")
+    print(f"Windows message: {str(msg)}, {datetime_str}")
+    print(f"rts_called:{rts_called}")
+    # if msg == win32con.WM_DESTROY:
+    #     if not rts_called:
+    #         rts.start()
+    #         rts_called = True
+    #     return True 
 
+    if msg in message_map:
+        # Handle the shutdown request here
+        if not rts_called:
+            rts.start()
+            rts_called = True
+        return True  # Return True to allow shutdown, False to deny it
+    
     return win32gui.DefWindowProc(hwnd, msg, wparam, lparam)
 
 def create_window():
@@ -33,12 +39,13 @@ def create_window():
     wc.lpszClassName = "ShutdownHandleFunc"
     wc.hInstance = win32api.GetModuleHandle(None)
     class_atom = win32gui.RegisterClass(wc)
-    hwnd = win32gui.CreateWindow(class_atom, "ShutdownHandlerWindow", 0, 0, 0, 0, 0, 0, 0, wc.hInstance, None)
+    hwnd = win32gui.CreateWindow(class_atom, "ShutdownHandlerWindow", 
+                                 0, 0, 0, 0, 0, 0, 0, wc.hInstance, None)
     return hwnd
 
 def main():
     hwnd = create_window()
-    print(f"window created with hwnd: {hwnd}") 
+    print(f"Window created with hwnd: {hwnd}") 
     win32gui.PumpMessages()
 
 if __name__ == "__main__":
