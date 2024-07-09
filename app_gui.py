a7a3240dda1e6295a11ef98bcdfd62115ea1dc1f
lusan23 <luanlol450@gmail.com>
2024-07-09 10:22:01 -0300

    added the time spent in the last week and month. now the total time spent is calculated by the sum of history.

--- app_gui.py
+++ app_gui.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import (QApplication, QWidget, QMainWindow, QLabel,
                              QVBoxLayout,  )
 from PyQt6.QtCore import Qt, QTimer
-from record_time_script import calc_time_spent, time_spent_to_string, load_total_time
+from time_session_utility import calc_time_spent, time_spent_to_string
+from time_history import calc_session_total_time, calc_total_time, get_time_from_sessions_total,calc_last_seven_days_total, calc_last_thirty_days_total
 import sys
 
 
@@ -12,11 +13,11 @@ class MainWindow(QMainWindow):
 
         super(MainWindow, self).__init__()
         self.setFixedSize(400, 300)
-
-        total_spent_time = load_total_time()
+   
+        total_spent_time = calc_total_time()
         if not total_spent_time == None:
-            total_spent_time_deltatime = total_spent_time[:14]
-            total_last_update_date = total_spent_time[15:]
+            total_spent_time: str = f"days:{total_spent_time['days']}, {total_spent_time['hours']}:{total_spent_time['minutes']}:{total_spent_time['seconds']}"
+            total_last_update_date = ""
         else:
             total_spent_time_deltatime = "None"
             total_last_update_date = "None"
@@ -28,18 +29,45 @@ class MainWindow(QMainWindow):
         font.setPointSize(14)
         self.total_time_spent.setFont(font)
         self.total_time_spent.setAlignment(Qt.AlignmentFlag.AlignCenter)
-    
-        self.total_time_spent.setText(f"TOTAL TIME SPENT: {total_spent_time_deltatime}")
-        layout.addWidget(self.total_time_spent)
 
-        # DATE OF THE LAST RECORD
-        self.date_last_record = QLabel("<DATE_LAST_RECORD>", self)
-        font = self.date_last_record.font()
-        font.setPointSize(12)
-        self.date_last_record.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.date_last_record.setText(f"LAST RECORD AT:{total_last_update_date}")
-        layout.addWidget(self.date_last_record)
         
+        self.total_time_spent.setText(f"TOTAL TIME SPENT: {total_spent_time},")
+        layout.addWidget(self.total_time_spent)
+        
+        FONT_SIZE = 14
+        # TODAY TOTAL  TIME 
+        today_total_time = calc_session_total_time(return_as_string=True)
+        today_total_time = get_time_from_sessions_total(today_total_time)
+
+        self.today_total_time = QLabel("<TODAY TIME SPENT>", self)
+        font = self.today_total_time.font()
+        font.setPointSize(FONT_SIZE)
+        self.today_total_time.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.today_total_time.setText(f"Today's Activity :{today_total_time}")
+        layout.addWidget(self.today_total_time)
+        
+        # LAST SEVEN DAYS TOTAL TIME 
+        week_total_time = calc_last_seven_days_total(return_as_string=True)
+        week_total_time = get_time_from_sessions_total(week_total_time)
+
+        self.last_week_total_time_total_time = QLabel("<LAST SEVEN DAYS TIME SPENT>", self)
+        font = self.last_week_total_time_total_time.font()
+        font.setPointSize(FONT_SIZE)
+        self.last_week_total_time_total_time.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.last_week_total_time_total_time.setText(f"Week's Activity :{week_total_time}")
+        layout.addWidget(self.last_week_total_time_total_time)
+
+        # LAST THIRTY DAYS TOTAL TIME
+        month_total_time = calc_last_thirty_days_total(return_as_string=True)
+        month_total_time = get_time_from_sessions_total(month_total_time)
+        
+        self.last_month_total_time = QLabel("<LAST THIRTY DAYS TIME SPENT>", self)
+        font = self.last_month_total_time.font()
+        font.setPointSize(FONT_SIZE)
+        self.last_month_total_time.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.last_month_total_time.setText(f"Month's Activity :{month_total_time}")
+        layout.addWidget(self.last_month_total_time)
+
         # CURRENT SESSION TIME SPENT LABEL
         self.current_session_time_spent = QLabel("<SPENT_TIME>", self)
         font = self.current_session_time_spent.font()
