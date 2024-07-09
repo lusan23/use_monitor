afcc73e11c4268d9bc1cbe8bf02a01aadfbb8136
lusan23 <luanlol450@gmail.com>
2024-07-09 11:28:25 -0300

    now the log history is updated at the startup

--- background_process.py
+++ background_process.py
@@ -1,14 +1,13 @@
 import win32api 
 import win32con
 import win32gui
-import record_time_script as rts
+import time_session_utility as rts
 import datetime
+from time_history import start, update_sessions_history
 
-# Flag to indicate if rts.start() has been called
 rts_called = False
 
 def wnd_proc(hwnd, msg, wparam, lparam):
-    global rts_called
     message_map = [win32con.WM_QUERYENDSESSION, win32con.WM_ENDSESSION]
 
     # Get current date and time
@@ -18,22 +17,21 @@ def wnd_proc(hwnd, msg, wparam, lparam):
     datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Windows message: {str(msg)}, {datetime_str}")
     print(f"rts_called:{rts_called}")
-    # if msg == win32con.WM_DESTROY:
-    #     if not rts_called:
-    #         rts.start()
-    #         rts_called = True
-    #     return True 
+ 
 
-    if msg in message_map:
+    if msg in message_map or msg == 799:
         # Handle the shutdown request here
-        if not rts_called:
-            rts.start()
-            rts_called = True
+        try:
+            start()
+        except Exception as e:
+            with open("./shutdown_log.txt", "a") as f:
+                f.write(str(e))
         return True  # Return True to allow shutdown, False to deny it
     
     return win32gui.DefWindowProc(hwnd, msg, wparam, lparam)
 
 def create_window():
+
     wc = win32gui.WNDCLASS()
     wc.lpfnWndProc = wnd_proc
     wc.lpszClassName = "ShutdownHandleFunc"
@@ -49,4 +47,7 @@ def main():
     win32gui.PumpMessages()
 
 if __name__ == "__main__":
+    update_sessions_history()
+    print("-"*20)
     main()
+  
